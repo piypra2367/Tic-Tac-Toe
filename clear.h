22dc2c8d0eee724b2c767f9de040f1a181e3f2eb
Piyush Raj <81475553+piypra2367@users.noreply.github.com>
2022-08-24 12:00:54 +0530

    Add files via upload

---
+++ clear.h
@@ -0,0 +1,6 @@
+#include<iostream>
+
+using namespace std;
+void clr(){
+    system("cls");
+}
---
+++ misc.hpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+// #include "clear.h"
+using namespace std;
+void details() {
+    clr();
+    cout<<"==================WELCOME TO THE TICTACTOE GAME==================\n";
+    cout<<"ENTER 1 =>  for 2 players\n";
+    cout<<"ENTER 2 =>  for playing with robot\n";
+    cout<<"ENTER 3 =>  for playing with robot(u can never win)\n";
+    cout<<"Enter your choice: ";
+}
+
+void defCase() {
+    cout<<"This feature is being programmed, it will be released soon soon!\nMake sure to keep an eye on the repository: https://github.com/ash-dodek/TicTacToe.git\nEnter a new choice:";
+}
